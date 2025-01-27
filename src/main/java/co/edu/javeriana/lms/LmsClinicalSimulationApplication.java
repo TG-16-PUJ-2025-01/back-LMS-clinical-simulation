99b96b6706a975504131063a3db78ceb1747f5b4
Santicm23 <santicastrom@outlook.com>
2025-01-26 21:56:23 -0500

    creating basic java 17 spring boot application

---
+++ src/main/java/co/edu/javeriana/lms/LmsClinicalSimulationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsClinicalSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsClinicalSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/lms/LmsClinicalSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsClinicalSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

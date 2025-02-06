e0aaa7c6e788880dc9cb8dc52dbe456a0e34bec3
Andrés García Montoya <andres345gm@gmail.com>
2025-02-05 21:23:40 -0500

    login changes

--- src/main/java/co/edu/javeriana/lms/controllers/AuthController.java
+++ src/main/java/co/edu/javeriana/lms/controllers/AuthController.java
@@ -3,7 +3,6 @@ package co.edu.javeriana.lms.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 

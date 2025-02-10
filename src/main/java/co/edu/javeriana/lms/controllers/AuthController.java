4771ca0bfc391ab277aa8806cd43b283c3ef87fb
Andrés García Montoya <andres345gm@gmail.com>
2025-02-09 19:37:47 -0500

    Refactor of password reset and auth

--- src/main/java/co/edu/javeriana/lms/controllers/AuthController.java
+++ src/main/java/co/edu/javeriana/lms/controllers/AuthController.java
@@ -6,9 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import co.edu.javeriana.lms.dtos.LoginDTO;
-import co.edu.javeriana.lms.dtos.PasswordResetDTO;
-import co.edu.javeriana.lms.models.PasswordResetToken;
-import co.edu.javeriana.lms.services.UserService;
+import co.edu.javeriana.lms.services.AuthService;
+
 import jakarta.validation.Valid;
 
 @RestController
@@ -16,14 +15,14 @@ import jakarta.validation.Valid;
 public class AuthController {
 
     @Autowired
-    private UserService userService;
+    private AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody LoginDTO loginDTO) {
         String email = loginDTO.getEmail();
         String password = loginDTO.getPassword();
         
-        String token = userService.login(email, password);
+        String token = authService.login(email, password);
         if (token == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
@@ -35,38 +34,10 @@ public class AuthController {
         String email = loginDTO.getEmail();
         String password = loginDTO.getPassword();
         
-        String token = userService.changePassword(email, password);
+        String token = authService.changePassword(email, password);
         if (token == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
         return ResponseEntity.ok(token);
-    }
-
-    @PostMapping("/request-password-reset")
-    public ResponseEntity<?> requestPasswordReset(@Valid @RequestBody String email) {
-        PasswordResetToken passwordResetToken = userService.createPasswordResetToken(email);
-        userService.sentPasswordResetEmail(email, passwordResetToken.getToken());
-        return ResponseEntity.ok("Password reset requested");
-    }
-
-    @PostMapping("/verify-password-reset")
-    public ResponseEntity<?> verifyPasswordReset(@Valid @RequestBody PasswordResetDTO passwordResetDTO) {
-        String email = passwordResetDTO.getEmail();
-        String token = passwordResetDTO.getToken();
-        if(userService.verifyResetToken(email, token)) {
-            return ResponseEntity.ok("Password reset verified");   
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
-    }
-
-    @PostMapping("/password-reset")
-    public ResponseEntity<?> resetPassword(@Valid @RequestBody PasswordResetDTO passwordResetDTO) {
-        
-        String email = passwordResetDTO.getEmail();
-        String token = passwordResetDTO.getToken();
-        String password = passwordResetDTO.getPassword();
-
-        userService.resetPassword(email, token, password);
-        return ResponseEntity.ok("Password reseted successfully");
     }
 }
--- src/main/java/co/edu/javeriana/lms/services/UserService.java
+++ src/main/java/co/edu/javeriana/lms/services/UserService.java
@@ -1,19 +1,13 @@
 package co.edu.javeriana.lms.services;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import co.edu.javeriana.lms.models.PasswordResetToken;
 import co.edu.javeriana.lms.models.User;
-import co.edu.javeriana.lms.repositories.PasswordResetTokenRepository;
 import co.edu.javeriana.lms.repositories.UserRepository;
 import co.edu.javeriana.lms.utils.PasswordGenerator;
 import jakarta.transaction.Transactional;
@@ -31,93 +25,6 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private EmailService emailService;
-
-    @Autowired
-    private JwtService jwtService;
-
-    @Autowired
-    private PasswordResetTokenRepository passwordResetTokenRepository;
-
-    public String login(String email, String password) {
-        log.info("Logging in user: " + email);
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found")); 
-
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
-        }
-
-        String token = jwtService.generateToken(user);
-        return token;
-    }
-
-    public String changePassword(String email, String password) {
-        log.info("Changing password for user: " + email);
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        user.setPassword(passwordEncoder.encode(password));
-        userRepository.save(user);
-        String token = jwtService.generateToken(user);
-
-        String subject = "Cambio de contraseña LMS";
-        String body = "Hola " + user.getEmail() + ",\n\nTu contraseña ha sido cambiada con éxito.\n" +
-                  "Si no fuiste tú, por favor, contacta al administrador.";
-        emailService.sendEmail(user.getEmail(), subject, body);
-
-        return token;
-    }
-
-    public PasswordResetToken createPasswordResetToken (String email) {
-        log.info("Creating password reset token for user: " + email);
-        if (!userRepository.existsByEmail(email)) {
-            throw new RuntimeException("User not found");
-        }
-        
-        String token = UUID.randomUUID().toString().substring(0, 6);
-        PasswordResetToken passwordResetToken = new PasswordResetToken();
-        passwordResetToken.setToken(token);
-        passwordResetToken.setUserEmail(email);
-        passwordResetToken.setExpirationDate(LocalDateTime.now().plusMinutes(30));
-        return passwordResetTokenRepository.save(passwordResetToken);
-    }
-
-    public void sentPasswordResetEmail(String email, String token) {
-        log.info("Sending password reset email to: " + email);
-
-        String subject = "Restablecer contraseña LMS";
-        String body = "Hola " + email + ",\n\nPara restablecer tu contraseña, utiliza el siguiente código: " + token + "\n\n" +
-                  "Si no solicitaste el cambio de contraseña, por favor, ignora este mensaje.";
-        emailService.sendEmail(email, subject, body);
-    }
-
-    public Boolean verifyResetToken (String email, String token) {
-        log.info("Verifying password reset token for user: " + email);
-        PasswordResetToken passwordResetToken = passwordResetTokenRepository.findByToken(token)
-                .orElseThrow(() -> new RuntimeException("Token not found"));
-        if (!passwordResetToken.getUserEmail().equals(email)) {
-            throw new RuntimeException("Invalid token");
-        }
-        if (passwordResetToken.isExpired()) {
-            throw new RuntimeException("Token expired");
-        }
-        return true;
-    }
-
-    public void resetPassword(String email, String token, String password) {
-        log.info("Resetting password for user: " + email);
-        if (!verifyResetToken(email, token)) {
-            throw new RuntimeException("Invalid token");
-        }
-        changePassword(email, password);
-    }
-
-    @Scheduled(fixedRate = 600000)
-    public void deleteExpiredTokens() {
-        passwordResetTokenRepository.deleteAllExpiredTokens(LocalDateTime.now());
-    }
-
-
 
     public User createUser(User user) {
         log.info("Creating user: " + user.getEmail());

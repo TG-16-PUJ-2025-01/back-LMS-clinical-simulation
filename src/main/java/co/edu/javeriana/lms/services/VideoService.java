d3f30d98d0c020ce9bfeeee65cafe9393df2afea
Santicm23 <santicastrom@outlook.com>
2025-02-14 21:42:02 -0500

    sorting by video Id

--- src/main/java/co/edu/javeriana/lms/services/VideoService.java
+++ src/main/java/co/edu/javeriana/lms/services/VideoService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import co.edu.javeriana.lms.dtos.EditVideoDTO;
@@ -19,7 +20,7 @@ public class VideoService {
     private VideoRepository videoRepository;
 
     public Page<Video> getAllVideos(Integer page, Integer size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("videoId"));
         return videoRepository.findAll(pageable);
     }
 

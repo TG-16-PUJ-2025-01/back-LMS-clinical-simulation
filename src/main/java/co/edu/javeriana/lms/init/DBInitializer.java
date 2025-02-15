a487ce512f0db041f0c89494d90dc0db21178aec
Santicm23 <santicastrom@outlook.com>
2025-02-15 09:13:40 -0500

    minor fix

--- src/main/java/co/edu/javeriana/lms/init/DBInitializer.java
+++ src/main/java/co/edu/javeriana/lms/init/DBInitializer.java
@@ -1,5 +1,6 @@
 package co.edu.javeriana.lms.init;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -7,15 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import co.edu.javeriana.lms.models.GradeStatus;
-import co.edu.javeriana.lms.models.Simulation;
-import co.edu.javeriana.lms.repositories.SimulationRepository;
+import co.edu.javeriana.lms.models.Room;
+import co.edu.javeriana.lms.models.RoomType;
+import co.edu.javeriana.lms.models.Video;
+import co.edu.javeriana.lms.repositories.RoomRepository;
+import co.edu.javeriana.lms.repositories.RoomTypeRepository;
+import co.edu.javeriana.lms.repositories.VideoRepository;
 
 @Component
 public class DBInitializer implements CommandLineRunner {
 
-        @Autowired
-        private VideoRepository videoRepository;
+    @Autowired
+    private VideoRepository videoRepository;
 
     @Autowired
     private RoomRepository roomRepository;
@@ -23,72 +27,11 @@ public class DBInitializer implements CommandLineRunner {
     @Autowired
     private RoomTypeRepository roomTypeRepository;
 
-        @Override
-        public void run(String... args) throws Exception {
-                Video video1 = new Video("javatechie.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2023-01-31"),
-                                new Date(), 62L, 8.3);
-
-                Video video2 = new Video("10350-224234500_small.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 600L, 31.2);
-
-                Video video3 = new Video("unavailable1.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 210L, 300.0);
-                video3.setAvailable(false);
-
-                Video video4 = new Video("unavailable2.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 450L, 420.0);
-                video4.setAvailable(false);
-
-                Video video5 = new Video("unavailable3.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 600L, 500.0);
-                video5.setAvailable(false);
-
-                Video video6 = new Video("unavailable4.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 780L, 780.0);
-                video6.setAvailable(false);
-
-                Video video7 = new Video("unavailable5.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 6000L, 6000.0);
-                video7.setAvailable(false);
-
-                Video video8 = new Video("unavailable6.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 620L, 500.0);
-                video8.setAvailable(false);
-                Video video9 = new Video("unavailable7.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 620L, 500.0);
-                video9.setAvailable(false);
-                Video video10 = new Video("unavailable8.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 620L, 500.0);
-                video10.setAvailable(false);
-                Video video11 = new Video("unavailable9.mp4",
-                                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
-                                new Date(), 620L, 500.0);
-                video11.setAvailable(false);
-
-                videoRepository.save(video1);
-                videoRepository.save(video2);
-                videoRepository.save(video3);
-                videoRepository.save(video4);
-                videoRepository.save(video5);
-                videoRepository.save(video6);
-                videoRepository.save(video7);
-                videoRepository.save(video8);
-                videoRepository.save(video9);
-                videoRepository.save(video10);
-                videoRepository.save(video11);
-
-            
+    @Override
+    public void run(String... args) throws Exception {
         insertRoomsAndTypes();
+
+        insertVideos();
     }
 
     public void insertRoomsAndTypes() {
@@ -163,4 +106,67 @@ public class DBInitializer implements CommandLineRunner {
         roomRepository.save(room10);
     }
 
+    public void insertVideos() throws ParseException {
+        Video video1 = new Video("javatechie.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2023-01-31"),
+                new Date(), 62L, 8.3);
+
+        Video video2 = new Video("10350-224234500_small.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 600L, 31.2);
+
+        Video video3 = new Video("unavailable1.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 210L, 300.0);
+        video3.setAvailable(false);
+
+        Video video4 = new Video("unavailable2.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 450L, 420.0);
+        video4.setAvailable(false);
+
+        Video video5 = new Video("unavailable3.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 600L, 500.0);
+        video5.setAvailable(false);
+
+        Video video6 = new Video("unavailable4.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 780L, 780.0);
+        video6.setAvailable(false);
+
+        Video video7 = new Video("unavailable5.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 6000L, 6000.0);
+        video7.setAvailable(false);
+
+        Video video8 = new Video("unavailable6.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 620L, 500.0);
+        video8.setAvailable(false);
+        Video video9 = new Video("unavailable7.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 620L, 500.0);
+        video9.setAvailable(false);
+        Video video10 = new Video("unavailable8.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 620L, 500.0);
+        video10.setAvailable(false);
+        Video video11 = new Video("unavailable9.mp4",
+                new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-31"),
+                new Date(), 620L, 500.0);
+        video11.setAvailable(false);
+
+        videoRepository.save(video1);
+        videoRepository.save(video2);
+        videoRepository.save(video3);
+        videoRepository.save(video4);
+        videoRepository.save(video5);
+        videoRepository.save(video6);
+        videoRepository.save(video7);
+        videoRepository.save(video8);
+        videoRepository.save(video9);
+        videoRepository.save(video10);
+        videoRepository.save(video11);
+    }
 }

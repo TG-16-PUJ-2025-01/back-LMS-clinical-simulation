695815fbe1d9f97bb5672e2f58e4a4d47a9cb540
Sophia Aristizabal Florez <67614769+sophiearisti@users.noreply.github.com>
2025-03-03 22:29:34 -0500

    refactor SecurityConfig to use Role enum for authority checks in access control

--- src/main/java/co/edu/javeriana/lms/config/security/SecurityConfig.java
+++ src/main/java/co/edu/javeriana/lms/config/security/SecurityConfig.java
@@ -50,30 +50,30 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
                         .access((authentication, context) -> new AuthorizationDecision(env.matchesProfiles("dev")))
                         .requestMatchers("/streaming/**").permitAll() // Public endpoint
-                        .requestMatchers("/admin/**").hasAuthority("ADMIN") // Admin-only
+                        .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.name()) // Admin-only
                         .requestMatchers("/profesor/**").hasAuthority("profesor") // Profesor-only
                         .requestMatchers("/estudiante/**").hasAuthority("estudiante") // Estudiante-only
                         .requestMatchers("/coordinador/**").hasAuthority("coordinador") // Coordinador-only
 
-                        .requestMatchers("/course/delete/**").hasAuthority("ADMIN") // Coordinador-only
-                        .requestMatchers("/course/add/**").hasAuthority("ADMIN") // Coordinador-only
-                        .requestMatchers("/course/update/**").hasAuthority("ADMIN") // Coordinador-only
-                        .requestMatchers("/course/all").hasAuthority("ADMIN") // Coordinador-only
-                        .requestMatchers("/course/{id}").hasAnyAuthority("ADMIN", "coordinador") // Coordinador-only
+                        .requestMatchers("/course/delete/**").hasAuthority(Role.ADMIN.name()) // Coordinador-only
+                        .requestMatchers("/course/add/**").hasAuthority(Role.ADMIN.name()) // Coordinador-only
+                        .requestMatchers("/course/update/**").hasAuthority(Role.ADMIN.name()) // Coordinador-only
+                        .requestMatchers("/course/all").hasAuthority(Role.ADMIN.name()) // Coordinador-only
+                        .requestMatchers("/course/{id}").hasAnyAuthority(Role.ADMIN.name(), "coordinador") // Coordinador-only
 
-                        .requestMatchers("/class/all").hasAnyAuthority("ADMIN")
+                        .requestMatchers("/class/all").hasAnyAuthority(Role.ADMIN.name())
                         .requestMatchers("/class/{id}").authenticated()
-                        .requestMatchers("/class/add").hasAnyAuthority("ADMIN", "profesor", "coordinador")
-                        .requestMatchers("/class/update/{id}").hasAnyAuthority("ADMIN", "profesor", "coordinador")
-                        .requestMatchers("/class/delete/{id}").hasAnyAuthority("ADMIN", "coordinador")
+                        .requestMatchers("/class/add").hasAnyAuthority(Role.ADMIN.name(), "profesor", "coordinador")
+                        .requestMatchers("/class/update/{id}").hasAnyAuthority(Role.ADMIN.name(), "profesor", "coordinador")
+                        .requestMatchers("/class/delete/{id}").hasAnyAuthority(Role.ADMIN.name(), "coordinador")
                         .requestMatchers("/class/{id}/member/all").authenticated()
                         .requestMatchers("/class/{id}/member/all/outside")
-                        .hasAnyAuthority("ADMIN", "profesor", "coordinador")
+                        .hasAnyAuthority(Role.ADMIN.name(), "profesor", "coordinador")
                         .requestMatchers("/class/delete/{id}/member/{idMember}")
-                        .hasAnyAuthority("ADMIN", "coordinador", "profesor")
+                        .hasAnyAuthority(Role.ADMIN.name(), "coordinador", "profesor")
                         .requestMatchers("/class/update/{id}/members")
-                        .hasAnyAuthority("ADMIN", "profesor", "coordinador")
-                        .requestMatchers("/class/add").hasAnyAuthority("ADMIN", "profesor", "coordinador")
+                        .hasAnyAuthority(Role.ADMIN.name(), "profesor", "coordinador")
+                        .requestMatchers("/class/add").hasAnyAuthority(Role.ADMIN.name(), "profesor", "coordinador")
 
                         .requestMatchers("/auth/login").permitAll() // Public endpoint
                         .requestMatchers("/auth/change-password").authenticated() // Authenticated endpoint

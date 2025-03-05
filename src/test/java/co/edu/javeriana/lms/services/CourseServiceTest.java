65c1e680baa5c1832e487a30b6c64a9c649efb6f
Andrés García Montoya <andres345gm@gmail.com>
2025-03-04 19:00:44 -0500

    add unit and integration tests for Class and Course controllers and services

---
+++ src/test/java/co/edu/javeriana/lms/services/CourseServiceTest.java
@@ -0,0 +1,10 @@
+package co.edu.javeriana.lms.services;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class CourseServiceTest {
+    
+}

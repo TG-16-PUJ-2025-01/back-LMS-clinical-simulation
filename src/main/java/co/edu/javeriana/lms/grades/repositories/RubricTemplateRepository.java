41ee02a9e11d7922c50179b94e92607039a59242
Santicm23 <santicastrom@outlook.com>
2025-03-23 14:20:33 -0500

    minor fixes on get rubrics

--- src/main/java/co/edu/javeriana/lms/grades/repositories/RubricTemplateRepository.java
+++ src/main/java/co/edu/javeriana/lms/grades/repositories/RubricTemplateRepository.java
@@ -19,12 +19,11 @@ public interface RubricTemplateRepository extends JpaRepository<RubricTemplate, 
         WHERE 
             (LOWER(TRANSLATE(c.title, 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU')) LIKE LOWER(CONCAT('%', TRANSLATE(:filter, 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU'), '%'))
             OR CAST(c.creationDate AS string) LIKE CONCAT('%', :filter, '%'))
-            AND c.archived = :archived
+            AND c.archived = true
             AND c.creator.id = :creatorId
     """)
     Page<RubricTemplate> findArchivedMineByTitleOrCreationDateContaining(
         @Param("filter") String filter, 
-        @Param("archived") Boolean archived, 
         @Param("creatorId") Long creatorId, 
         Pageable pageable
     );
@@ -59,13 +58,23 @@ public interface RubricTemplateRepository extends JpaRepository<RubricTemplate, 
         WHERE p.id = :id
     """)
     List<RubricTemplate> findRecommendedRubricTemplatesByCoursesById(@Param("id") Long id);
-    
+
     @Query("""
         SELECT c FROM RubricTemplate c 
         WHERE 
             (LOWER(TRANSLATE(c.title, 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU')) LIKE LOWER(CONCAT('%', TRANSLATE(:filter, 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU'), '%'))
             OR CAST(c.creationDate AS string) LIKE CONCAT('%', :filter, '%'))
+            AND c.archived = false
     """)
-    Page<RubricTemplate> findAllByTitleOrCreationDateContaining(@Param("filter") String filter, Pageable pageable);
+    Page<RubricTemplate> findNotArchivedByTitleOrCreationDateContaining(@Param("filter") String filter, Pageable pageable);
+
+    @Query("""
+        SELECT c FROM RubricTemplate c 
+        WHERE 
+            (LOWER(TRANSLATE(c.title, 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU')) LIKE LOWER(CONCAT('%', TRANSLATE(:filter, 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU'), '%'))
+            OR CAST(c.creationDate AS string) LIKE CONCAT('%', :filter, '%'))
+            AND c.archived = true
+    """)
+    Page<RubricTemplate> findArchivedByTitleOrCreationDateContaining(@Param("filter") String filter, Pageable pageable);
 
 }

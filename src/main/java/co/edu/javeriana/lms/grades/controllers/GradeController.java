55c3761270ce27908a141d5c23665b93a8477afe
Andrés García Montoya <andres345gm@gmail.com>
2025-04-17 13:23:53 -0500

    fix: remove redundant final grades retrieval in getStudentGradeByToken method

--- src/main/java/co/edu/javeriana/lms/grades/controllers/GradeController.java
+++ src/main/java/co/edu/javeriana/lms/grades/controllers/GradeController.java
@@ -3,7 +3,6 @@ package co.edu.javeriana.lms.grades.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,7 +52,6 @@ public class GradeController {
                 200,
                 "ok",
                 studentGrades,
-                gradeService.getFinalGradesByClass(classId),
                 null));
     }
 

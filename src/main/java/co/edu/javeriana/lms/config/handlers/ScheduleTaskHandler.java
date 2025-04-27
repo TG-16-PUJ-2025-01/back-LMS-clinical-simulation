f34a567367de5070e04704a6f2163b69673ddb88
Santicm23 <santicastrom@outlook.com>
2025-04-26 21:05:53 -0500

    creating basic service and schedule handler for arec sync method

---
+++ src/main/java/co/edu/javeriana/lms/config/handlers/ScheduleTaskHandler.java
@@ -0,0 +1,26 @@
+package co.edu.javeriana.lms.config.handlers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import co.edu.javeriana.lms.videos.services.ArecService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ScheduleTaskHandler {
+
+    @Autowired
+    private ArecService arecService;
+
+    // @Scheduled(cron = "0 0 6 * * *") // Cron expression for running every minute
+    @Scheduled(cron = "0 * * * * *") // Cron expression for running every minute
+    public void syncArecVideos() throws URISyntaxException, IOException, InterruptedException {
+        log.info("Scheduled task to sync videos with Arec started");
+        arecService.syncVideos();
+    }
+}

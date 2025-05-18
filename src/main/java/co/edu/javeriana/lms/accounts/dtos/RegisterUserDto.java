bdc3b5205aca929e468aacd59dba202704356041
santicm23 <santicastrom@outlook.com>
2025-05-18 18:03:25 -0500

    adding user integration tests

--- src/main/java/co/edu/javeriana/lms/accounts/dtos/RegisterUserDto.java
+++ src/main/java/co/edu/javeriana/lms/accounts/dtos/RegisterUserDto.java
@@ -10,12 +10,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class RegisterUserDto {
 
     @NotBlank(message = "Email is required")
